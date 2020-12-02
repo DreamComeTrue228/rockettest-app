7dc9cdc41f6c83f1b24db76ed99618d41e0daa99
An Yury <dreamcometrue228@gmail.com>
2020-12-02 09:16:45 +0600

    update README

--- public/js/app.js
+++ public/js/app.js
@@ -50999,8 +50999,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/toshiba/php/test-projects/rockettest-app/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/toshiba/php/test-projects/rockettest-app/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/toshiba/php/rockettest-app/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/toshiba/php/rockettest-app/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
